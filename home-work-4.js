fba5aa8f2e7d7a2e1d69939f378170a5fd7185e4
Illia BAIEV <bayev.illya@gmail.com>
2024-01-20 18:59:24 +0200

    Front End Pro - Home work 4

---
+++ home-work-4.js
@@ -0,0 +1,6 @@
+let firstNumber = +prompt('Введіть перше число');
+let secondNumber = +prompt('Введіть друге число');
+alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}\n
+${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}\n
+${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}\n
+${firstNumber} / ${secondNumber} = ${firstNumber/secondNumber}`);
