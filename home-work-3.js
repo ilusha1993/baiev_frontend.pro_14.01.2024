f5db73b54b615d9c88ba85e7a7062e76ef75b8ee
Illia BAIEV <bayev.illya@gmail.com>
2024-01-20 18:29:44 +0200

    Front End Pro - Home work 3

---
+++ home-work-3.js
@@ -0,0 +1,4 @@
+let firstNumber = +prompt('Введіть перше число');
+let secondNumber = +prompt('Введіть друге число');
+let thirdNumber = +prompt('Введіть третє число');
+alert(`Середнє арифметичне = ${(firstNumber + secondNumber + thirdNumber)/3}`);
