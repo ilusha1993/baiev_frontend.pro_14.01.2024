db8b1cc7a3b1792d7b6e1ffbdebfc708efbf7ca4
Illia BAIEV <bayev.illya@gmail.com>
2024-02-12 21:39:52 +0200

    Front End Pro - Home work 18

--- home-work-18.js
+++ home-work-18.js
@@ -5,15 +5,23 @@ document.body.appendChild(table);
 const tBody = document.createElement("tbody");
 table.appendChild(tBody);
 
+tBody.style.display = 'flex';
+tBody.style.flexDirection = 'column';
+tBody.style.gap = '2px';
+
 let count = 1;
 for (let i = 0; i < 10; i++){
     let row = document.createElement("tr");
-
+    row.style.display = 'flex';
+    row.style.gap = '2px';
     for (let j = 0; j < 10; j++){
         let cell = document.createElement("td");
         cell.style.border = '1px solid black';
-        cell.style.textAlign = 'center';
-        cell.style.padding = '15px';
+        cell.style.display = 'flex';
+        cell.style.alignItems = 'center';
+        cell.style.justifyContent = 'center';
+        cell.style.width = '50px';
+        cell.style.height = '50px';
         cell.textContent = count;
         count++;
         row.appendChild(cell);
